71874ce26d59ff2ceaa872c0c1f1ac57c8b457da
leitianjian <leitianjian@outlook.com>
2022-11-18 22:40:56 +0800

    update collision detection logic for performance reason

--- src/collision/collision.cpp
+++ src/collision/collision.cpp
@@ -28,10 +28,13 @@ struct Collision::Imp {
   Collision* collision_;
   std::thread collision_thread_;
   std::mutex collision_mutex_;
+  fcl::BroadPhaseCollisionManager* dynamic_tree_;
   std::array<double, 7 * 7> link_pq;
   size_t num_contacts;
 
-  Imp(Collision* collision) : collision_(collision) {
+  Imp(Collision* collision)
+      : collision_(collision),
+        dynamic_tree_(new fcl::DynamicAABBTreeCollisionManager()) {
     num_contacts = 0;
     link_pq = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
                0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
@@ -42,10 +45,10 @@ struct Collision::Imp {
 
 Collision::Collision(const std::string& robot_stl_path) : imp_(new Imp(this)) {
   std::vector<std::string> robot_stl_names;
-  //获取该路径下的所有文件
+  // 获取该路径下的所有文件
   GetFileName(robot_stl_path, robot_stl_names);
 
-  std::vector<fcl::CollisionObject> co_robot;
+  std::vector<std::unique_ptr<fcl::CollisionObject>> co_robot;
   for (int i = 0; i < robot_stl_names.size(); ++i) {
     std::cout << robot_stl_names[i] << std::endl;
     // Configure robot geometry.
@@ -55,9 +58,13 @@ Collision::Collision(const std::string& robot_stl_path) : imp_(new Imp(this)) {
     std::shared_ptr<Model> bvhmodel = std::make_shared<Model>();
     fcl::Vec3f scale{1, 1, 1};
     fcl::internal::meshFromAssimpScene(scale, loader.scene, bvhmodel);
-    fcl::CollisionObject co_link(bvhmodel);
-    co_robot.push_back(co_link);
+    std::unique_ptr<fcl::CollisionObject> co_link =
+        std::make_unique<fcl::CollisionObject>(bvhmodel);
+    imp_->dynamic_tree_->registerObject(co_link.get());
+    co_robot.push_back(std::move(co_link));
   }
+  // imp_->dynamic_tree_->update();
+  imp_->dynamic_tree_->setup();
 
   // Configure sphere geometry.
   const fcl::FCL_REAL r = 0.030;
@@ -68,24 +75,24 @@ Collision::Collision(const std::string& robot_stl_path) : imp_(new Imp(this)) {
 
   imp_->collision_thread_ = std::thread(
       [](size_t& num_contacts, std::array<double, 7 * 7>& link_pq,
-         std::vector<fcl::CollisionObject> co_robot,
-         fcl::CollisionObject sphere) {
+         std::vector<std::unique_ptr<fcl::CollisionObject>> co_robot,
+         fcl::CollisionObject sphere,
+         fcl::BroadPhaseCollisionManager* dynamic_tree) {
         // thread
         while (num_contacts == 0) {
+          // std::cout << "check collision" << std::endl;
           std::this_thread::sleep_for(std::chrono::milliseconds(10));
-          fcl::BroadPhaseCollisionManager* manager_robot =
-              new fcl::DynamicAABBTreeCollisionManager();
 
           for (int i = 1; i < co_robot.size(); ++i) {
-            co_robot[i].setTransform(
+            co_robot[i]->setTransform(
                 fcl::Quaternion3f(link_pq[i * 7 + 6], link_pq[i * 7 + 3],
                                   link_pq[i * 7 + 4], link_pq[i * 7 + 5])
                     .matrix(),
                 fcl::Vec3f(link_pq[i * 7], link_pq[i * 7 + 1],
                            link_pq[i * 7 + 2]));
-            co_robot[i].computeAABB();
-            manager_robot->registerObject(&co_robot[i]);
+            co_robot[i]->computeAABB();
           }
+          dynamic_tree->update();
 
           fcl::CollisionCallBackDefault collision_data;
           fcl::DistanceCallBackDefault distance_data;
@@ -93,17 +100,15 @@ Collision::Collision(const std::string& robot_stl_path) : imp_(new Imp(this)) {
           fcl::CollisionRequest co_req(fcl::CollisionRequestFlag::CONTACT, 1);
           co_req.security_margin = fcl::FCL_REAL(1e-2);
           collision_data.data.request = co_req;
-          manager_robot->setup();
-          manager_robot->collide(&sphere, &collision_data);
-          manager_robot->distance(&sphere, &distance_data);
-
+          dynamic_tree->collide(&sphere, &collision_data);
+          dynamic_tree->distance(&sphere, &distance_data);
           num_contacts = collision_data.data.result.numContacts();
           std::vector<fcl::Contact> contacts;
           collision_data.data.result.getContacts(contacts);
           double min_distance = distance_data.data.result.min_distance;
 
-          //std::cout << "min_distance: " << min_distance << std::endl;
-          //std::cout << "num_contacts: " << num_contacts << std::endl;
+          // std::cout << "min_distance: " << min_distance << std::endl;
+          // std::cout << "num_contacts: " << num_contacts << std::endl;
 
           if (num_contacts) {
             std::cout << "num " << contacts.size() << " contacts found"
@@ -114,12 +119,10 @@ Collision::Collision(const std::string& robot_stl_path) : imp_(new Imp(this)) {
               std::cout << "min_distance: " << min_distance << std::endl;
             }
           }
-
-          delete manager_robot;
         }  // thread_while
       },
       std::ref(imp_->num_contacts), std::ref(imp_->link_pq),
-      std::move(co_robot), std::move(sphere));
+      std::move(co_robot), std::move(sphere), imp_->dynamic_tree_);
 }
 Collision::~Collision() {}
 
@@ -130,9 +133,9 @@ auto Collision::instance(const std::string& robot_stl_path) -> Collision& {
 
 auto Collision::GetFileName(const std::string& path,
                             std::vector<std::string>& files) -> void {
-  //文件句柄
+  // 文件句柄
   intptr_t hFile;
-  //文件信息
+  // 文件信息
   struct _finddata_t fileinfo;
   std::string p;
   if ((hFile = _findfirst(p.assign(path).append("/*.STL").c_str(),
@@ -142,7 +145,7 @@ auto Collision::GetFileName(const std::string& path,
   } else {
     do {
       if ((fileinfo.attrib & _A_SUBDIR)) {
-        //读取子目录文件
+        // 读取子目录文件
         /*if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") !=
           0) GetFileName(p.assign(path).append("/").append(fileinfo.name),
           files); */
